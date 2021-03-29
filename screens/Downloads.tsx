7ed9385b10546d9f071ed7264942e9836a31a168
KishanItaliya <kishanpitaliya@gmail.com>
2021-03-29 14:45:59 +0530

    Authentication

--- screens/Downloads.tsx
+++ screens/Downloads.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
+import { Auth } from "aws-amplify";
 
 const Downloads = () => {
+  const onLogout = () => {
+    Auth.signOut();
+  };
   return (
-    <View>
-      <Text>Downloads Screen</Text>
+    <View style={{ alignItems: "center" }}>
+      <Pressable
+        onPress={onLogout}
+        style={{
+          padding: 10,
+          backgroundColor: "white",
+          borderRadius: 10,
+          marginTop: 10,
+        }}
+      >
+        <Text style={{ color: "black" }}>Logout</Text>
+      </Pressable>
     </View>
   );
 };
